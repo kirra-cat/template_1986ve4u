dfc30b0f95b30ada9131e89af751c9d208edd3a6
Sergey S.Sklyarov <sergey.s.sklyarov@gmail.com>
2017-02-16 01:12:12 +0400

    Проект собирается, но работает ли - это сейчас для меня загадка.

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * main.c
+ *
+ *  Created on: 16 февр. 2017 г.
+ *      Author: kirra
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "MDR1986BE4.h"
+
+int main(void)
+{
+	while (true) {}
+	return 0;
+}
